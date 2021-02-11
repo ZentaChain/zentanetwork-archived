94522d469ca1cda3e5548bbae477ca1f219643d4
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2021-02-11 07:02:33 +0100

    Update lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -121,8 +121,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("zenta"),
-	impl_name: create_runtime_str!("zenta"),
+	spec_name: create_runtime_str!("zentachain"),
+	impl_name: create_runtime_str!("zentachain"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
