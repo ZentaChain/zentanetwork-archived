ef274cd48cd94cd6394ece8d8d0836a69e94d30e
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2021-03-23 18:54:46 +0100

    Update lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -275,7 +275,7 @@ parameter_types! {
 	pub const MaxLocks: u32 = 50;
 }
 
-impl pallet_balances::Trait for Runtime {
+impl pallet_balances::Config for Runtime {
 	type MaxLocks = MaxLocks;
 	/// The type for recording an account's balance.
 	type Balance = Balance;
@@ -284,7 +284,7 @@ impl pallet_balances::Trait for Runtime {
 	type DustRemoval = ();
 	type ExistentialDeposit = ExistentialDeposit;
 	type AccountStore = System;
-	type WeightInfo = ();
+	type WeightInfo = weights::pallet_balances::WeightInfo<Runtime>;
 }
 
 parameter_types! {
