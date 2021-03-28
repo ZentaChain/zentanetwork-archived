ad69277697e417ebae53e47ca36cc3a1fc0e307c
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2021-03-28 18:39:55 +0200

    Update constants.rs

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -1,8 +1,9 @@
+/// Money matters.
 pub mod currency {
 	use node_primitives::Balance;
 	pub const CHAINS: Balance = 1_000_000_000_000;
 	pub const DOLLARS: Balance = CHAINS / 100;       // 10_000_000_000
-	pub const CENTS: Balance = DOLLARS / 100;      // 100_000_000
+	pub const CENTS: Balance = DOLLARS / 100;       // 100_000_000
 	pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 20 * DOLLARS + (bytes as Balance) * 100 * MILLICENTS
