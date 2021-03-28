b01316281d824649980d190c62ff4bc121e3a6c8
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2021-03-28 23:58:16 +0200

    Update constants.rs

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -1,9 +1,9 @@
 /// Money matters.
 pub mod currency {
-	use node_primitives::Balance;
+	use zentachain_primitives::Balance;
 	pub const CHAINS: Balance = 1_000_000_000_000;
 	pub const DOLLARS: Balance = CHAINS / 100;       // 10_000_000_000
-	pub const CENTS: Balance = DOLLARS / 100;       // 100_000_000
+	pub const CENTS: Balance = DOLLARS / 100;      // 100_000_000
 	pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 20 * DOLLARS + (bytes as Balance) * 100 * MILLICENTS
@@ -12,7 +12,7 @@ pub mod currency {
 
 /// Time and blocks.
 pub mod time {
-	use node_primitives::{BlockNumber, Moment};
+	use zentachain_primitives::{BlockNumber, Moment};
 	pub const MILLISECS_PER_BLOCK: Moment = 6000;
 	pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
 	pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 4 * HOURS;
